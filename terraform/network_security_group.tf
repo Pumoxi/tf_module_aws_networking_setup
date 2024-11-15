bc08eff18d668a27ad5be844a5c427b49f217e33
couch05velvets <148357138+couch05velvets@users.noreply.github.com>
2024-11-15 14:48:48 +0800

    update security group

--- terraform/network_security_group.tf
+++ terraform/network_security_group.tf
@@ -4,31 +4,3 @@ resource "aws_security_group" "default_security_group" {
   vpc_id      = aws_vpc.vpc.id
 
 }
-
-resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
-  security_group_id = aws_security_group.default_security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.default_security_group.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = 22
-  ip_protocol = "tcp"
-  to_port = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.default_security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.default_security_group.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
